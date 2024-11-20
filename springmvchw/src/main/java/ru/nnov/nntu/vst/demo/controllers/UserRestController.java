49525e7421c1d7b11f1fd4dfccf7d313ccb7f7f5
Данилина Света <106675351+DanilinaSveta@users.noreply.github.com>
2024-11-20 17:56:23 +0300

    Add files via upload

---
+++ springmvchw/src/main/java/ru/nnov/nntu/vst/demo/controllers/UserRestController.java
@@ -0,0 +1,54 @@
+package ru.nnov.nntu.vst.demo.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import ru.nnov.nntu.vst.demo.models.User;
+import ru.nnov.nntu.vst.demo.services.UserCRUDService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserRestController {
+
+    UserCRUDService userCRUDService = new UserCRUDService();
+
+    @GetMapping("/user")
+    public User getUser() {
+        User user = new User(1, "Админ", "admin@nntu.ru");
+        return user;
+    }
+
+ /*   @GetMapping("/users")
+    public List<User> getUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "Админ", "admin@nntu.ru"));
+        users.add(new User(2, "Админ2", "admin2@nntu.ru"));
+
+        return users;
+    }*/
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userCRUDService.getUserDb();
+    }
+
+    @PostMapping("/user/{name}/{email}")
+    public void addUsers(@PathVariable String name, @PathVariable String email) {
+        userCRUDService.insert(new User(userCRUDService.getLastId()+1, name, email));
+        System.out.println("Добавлен пользователь "+
+                            userCRUDService.getUserDb().get(userCRUDService.getUserDb().size() - 1));
+    };
+    @DeleteMapping("/user/{id}")
+    public void deleteUsers(@PathVariable int id){
+        userCRUDService.delete(id);
+        System.out.println("Удален пользователь "+
+                userCRUDService.getUserDb().get(userCRUDService.getUserDb().size() - 1));
+    }
+
+    @PutMapping("/user/{id}/{name}/{email}")
+    public void updateUsers(@PathVariable int id, @PathVariable String name, @PathVariable String email) {
+        userCRUDService.update(id-1, new User(id, name, email));
+        System.out.println("Обнавлен пользователь "+
+                userCRUDService.getUserDb().get(userCRUDService.getUserDb().size() - 1));
+    };
+}
